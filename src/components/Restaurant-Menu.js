7a6ba151f6bf92b36c2337302a389d99dc3976fc
devish14 <devishreeramachandran@gmail.com>
2025-02-14 14:43:51 -0800

    #custom hooks

--- src/components/Restaurant-Menu.js
+++ src/components/Restaurant-Menu.js
@@ -8,7 +8,6 @@ const RestaurantMenu = () => {
   const { id } = useParams();
   console.log(id);
 
-
   // Adding Custom hooks here so fetching data is done by the custom hook and displaying data is done by Restaurant Menu component;
 
   const restaurantData = useRestaurantMenu(id);
@@ -29,8 +28,13 @@ const RestaurantMenu = () => {
     restaurantData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
       ?.card.itemCards ||
     restaurantData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-      ?.card.itemCards ||  restaurantData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card
-      ?.card.itemCards ;
+      ?.card.itemCards ||
+    restaurantData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card
+      ?.card.itemCards;
+
+  const itemCards1 =
+    restaurantData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[6]?.card
+      ?.card.categories || [];
 
   return (
     <div className="restaurant-menu">
@@ -85,47 +89,92 @@ const RestaurantMenu = () => {
         {itemCards.map((item) => {
           return (
             <div className="menu-data-wrapper" key={item?.card?.info?.id}>
-                <div className="container-1">
-                  <div className="data1">{item?.card?.info?.name}</div>
-                  <div className="data1">{item?.card?.info?.price / 100}</div>
-                  <div className="data1">
-                    <svg
-                      width="14"
-                      height="14"
-                      viewBox="0 0 14 14"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      role="img"
-                      fillColor="#116649"
-                    >
-                      <rect width="14" height="14" fill="white"></rect>
-                      <path
-                        d="M5.67163 3.99166C6.22068 2.34179 6.49521 1.51686 7 1.51686C7.50479 1.51686 7.77932 2.34179 8.32837 3.99166L8.65248 4.96556H9.60668C11.4122 4.96556 12.315 4.96556 12.4703 5.45302C12.6256 5.94049 11.8893 6.4628 10.4167 7.50744L9.67376 8.03444L9.97544 8.94095C10.5325 10.615 10.8111 11.452 10.4033 11.754C9.99553 12.056 9.27604 11.5457 7.83705 10.5249L7 9.93112L6.16295 10.5249C4.72396 11.5457 4.00447 12.056 3.5967 11.754C3.18893 11.452 3.46747 10.615 4.02456 8.94095L4.04557 8.87783C4.18081 8.47145 4.24843 8.26825 4.18684 8.08006C4.12525 7.89187 3.94958 7.76725 3.59824 7.51802C2.11566 6.46633 1.37437 5.94049 1.52971 5.45302C1.68504 4.96556 2.5878 4.96556 4.39332 4.96556H5.34752L5.67163 3.99166Z"
-                        fill="#116649"
-                      ></path>
-                    </svg>
-                    <span className="data3">
-                      {item?.card?.info?.ratings?.aggregatedRating?.rating} (
-                      {
-                        item?.card?.info?.ratings?.aggregatedRating
-                          ?.ratingCountV2
-                      }
-                      )
-                    </span>
-                  </div>
-                  <div className="data4">{item?.card?.info?.description}</div>
+              <div className="container-1">
+                <div className="data1">{item?.card?.info?.name}</div>
+                <div className="data1"> ₹ {item?.card?.info?.price / 100}</div>
+                <div className="data1">
+                  <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    fillColor="#116649"
+                  >
+                    <rect width="14" height="14" fill="white"></rect>
+                    <path
+                      d="M5.67163 3.99166C6.22068 2.34179 6.49521 1.51686 7 1.51686C7.50479 1.51686 7.77932 2.34179 8.32837 3.99166L8.65248 4.96556H9.60668C11.4122 4.96556 12.315 4.96556 12.4703 5.45302C12.6256 5.94049 11.8893 6.4628 10.4167 7.50744L9.67376 8.03444L9.97544 8.94095C10.5325 10.615 10.8111 11.452 10.4033 11.754C9.99553 12.056 9.27604 11.5457 7.83705 10.5249L7 9.93112L6.16295 10.5249C4.72396 11.5457 4.00447 12.056 3.5967 11.754C3.18893 11.452 3.46747 10.615 4.02456 8.94095L4.04557 8.87783C4.18081 8.47145 4.24843 8.26825 4.18684 8.08006C4.12525 7.89187 3.94958 7.76725 3.59824 7.51802C2.11566 6.46633 1.37437 5.94049 1.52971 5.45302C1.68504 4.96556 2.5878 4.96556 4.39332 4.96556H5.34752L5.67163 3.99166Z"
+                      fill="#116649"
+                    ></path>
+                  </svg>
+                  <span className="data3">
+                    {item?.card?.info?.ratings?.aggregatedRating?.rating} (
+                    {item?.card?.info?.ratings?.aggregatedRating?.ratingCountV2}
+                    )
+                  </span>
                 </div>
-                <div className="container-2">
-                  <img
-                    className="image-align"
-                    src={SINGLE_IMAGE + item?.card?.info?.imageId}
-                    alt={item?.card?.info?.name}
-                  ></img>
-                </div>
-              
+                <div className="data4">{item?.card?.info?.description}</div>
+              </div>
+              <div className="container-2">
+                <img
+                  className="image-align"
+                  src={SINGLE_IMAGE + item?.card?.info?.imageId}
+                  alt={item?.card?.info?.name}
+                ></img>
+              </div>
             </div>
           );
         })}
+      </div>
+      <div className="menu-data-container1">
+        {itemCards1.map((category) => (
+          <div className="menu-data-wrapper1" key={category?.title}>
+            <div className="data1">{category?.title}</div>
+
+            {/* Loop through itemCards inside each category */}
+            {category?.itemCards?.map((items) => {
+              const info = items?.card?.info; // Extract 'info' for cleaner access
+              return (
+                <div key={info?.id} className="menu-item1">
+                  <div className="container-1">
+                    <div className="align1">{info?.name}</div>
+                    <div className="align1">₹ {info?.price / 100}</div>
+                    <div className="align1">
+                    <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    fillColor="#116649"
+                  >
+                    <rect width="14" height="14" fill="white"></rect>
+                    <path
+                      d="M5.67163 3.99166C6.22068 2.34179 6.49521 1.51686 7 1.51686C7.50479 1.51686 7.77932 2.34179 8.32837 3.99166L8.65248 4.96556H9.60668C11.4122 4.96556 12.315 4.96556 12.4703 5.45302C12.6256 5.94049 11.8893 6.4628 10.4167 7.50744L9.67376 8.03444L9.97544 8.94095C10.5325 10.615 10.8111 11.452 10.4033 11.754C9.99553 12.056 9.27604 11.5457 7.83705 10.5249L7 9.93112L6.16295 10.5249C4.72396 11.5457 4.00447 12.056 3.5967 11.754C3.18893 11.452 3.46747 10.615 4.02456 8.94095L4.04557 8.87783C4.18081 8.47145 4.24843 8.26825 4.18684 8.08006C4.12525 7.89187 3.94958 7.76725 3.59824 7.51802C2.11566 6.46633 1.37437 5.94049 1.52971 5.45302C1.68504 4.96556 2.5878 4.96556 4.39332 4.96556H5.34752L5.67163 3.99166Z"
+                      fill="#116649"
+                    ></path>
+                  </svg>
+                      <span className="align3">
+                        {info?.ratings?.aggregatedRating?.rating} (
+                        {info?.ratings?.aggregatedRating?.ratingCountV2})
+                      </span>
+                    </div>
+                    <div className="align4">{info?.description}</div>
+                  </div>
+                  <div className="container-2">
+                    <img
+                      className="image-align"
+                      src={SINGLE_IMAGE + info?.imageId}
+                      alt={info?.name}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
